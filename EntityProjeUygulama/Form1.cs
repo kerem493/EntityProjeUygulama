d5319f8423231529d4d4cf3ffb1cb63806ac4c08
Kerem Korkmaz <kerem.493@hotmail.com>
2023-05-06 14:30:18 +0300

    Entity Framework ve Linq sorgular ile yapılmış Sql veritabanı destekli uygulama

--- EntityProjeUygulama/Form1.cs
+++ EntityProjeUygulama/Form1.cs
@@ -16,5 +16,39 @@ namespace EntityProjeUygulama
         {
             InitializeComponent();
         }
+
+        DbEntityUrunEntities db = new DbEntityUrunEntities();
+        private void BtnListele_Click(object sender, EventArgs e)
+        {
+            var kategoriler = db.TBLKATEGORI.ToList();
+            dataGridView1.DataSource = kategoriler;
+        }
+
+        private void BtnEkle_Click(object sender, EventArgs e)
+        {
+            TBLKATEGORI t = new TBLKATEGORI();
+            t.AD = textBox2.Text;
+            db.TBLKATEGORI.Add(t);
+            db.SaveChanges();
+            MessageBox.Show("Kategori eklendi.");
+        }
+
+        private void BtnSil_Click(object sender, EventArgs e)
+        {
+            int x = Convert.ToInt32(textBox1.Text);
+            var ktgr = db.TBLKATEGORI.Find(x);
+            db.TBLKATEGORI.Remove(ktgr);
+            db.SaveChanges();
+            MessageBox.Show("Ürün silindi.");
+        }
+
+        private void BtnGuncelle_Click(object sender, EventArgs e)
+        {
+            int x = Convert.ToInt32(textBox1.Text);
+            var ktgr = db.TBLKATEGORI.Find(x);
+            ktgr.AD = textBox2.Text;
+            db.SaveChanges();
+            MessageBox.Show("Güncellendi.");
+        }
     }
 }
--- EntityProjeUygulama/Program.cs
+++ EntityProjeUygulama/Program.cs
@@ -16,7 +16,7 @@ namespace EntityProjeUygulama
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FrmGiris());
         }
     }
 }
